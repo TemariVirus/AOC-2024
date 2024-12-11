67bb6b4c130826af99af6e320106a988a041d668
TEMARI VÍRUS <kiyonnxperiaz@gmail.com>
2024-12-11 15:10:39 +0800

    Clean up day 11

--- exe/Day11.hs
+++ exe/Day11.hs
@@ -4,33 +4,30 @@ import Data.Int (Int64)
 import Data.Map (Map)
 import Data.Map qualified as Map
 
-parseInput :: String -> [Int64]
-parseInput = map read . words
+parseInput :: String -> Map Int64 Int64
+parseInput = Map.fromListWith (+) . map ((,1) . read) . words
 
-digits :: Int64 -> Int
-digits = length . show
+blink :: Map Int64 Int64 -> Map Int64 Int64
+blink counts =
+  let go stone
+        | stone == 0 = [1]
+        | even l = [stone `div` base, stone `mod` base]
+        | otherwise = [stone * 2024]
+        where
+          l = length $ show stone
+          base = 10 ^ (l `div` 2)
+   in Map.fromListWith
+        (+)
+        [ (stone', count)
+          | (stone, count) <- Map.toList counts,
+            stone' <- go stone
+        ]
 
-blinks :: Map (Int, Int64) Int64 -> Int -> Int64 -> (Map (Int, Int64) Int64, Int64)
-blinks m 0 _ = (m, 1)
-blinks m steps stone = case Map.lookup key m of
-  Just res -> (m, res)
-  Nothing -> (newM, res')
-  where
-    key = (steps, stone)
-    l = digits stone
-    base = 10 ^ (l `div` 2)
-    steps' = steps - 1
-    (m''', res')
-      | stone == 0 = blinks m steps' 1
-      | even l =
-          let (m', left) = blinks m steps' (stone `div` base)
-              (m'', right) = blinks m' steps' (stone `mod` base)
-           in (m'', left + right)
-      | otherwise = blinks m steps' (stone * 2024)
-    newM = Map.insert key res' m'''
+part :: Map Int64 Int64 -> Int -> Int64
+part counts = sum . map snd . Map.toList . (iterate blink counts !!)
 
 part1 :: String -> Int64
-part1 input = sum $ map (snd . blinks Map.empty 25) (parseInput input)
+part1 input = part (parseInput input) 25
 
 part2 :: String -> Int64
-part2 input = sum $ map (snd . blinks Map.empty 75) (parseInput input)
+part2 input = part (parseInput input) 75
